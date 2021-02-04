2957aca0c69a70947bed59194bc4c2af5dbe4f76
Roisin Bent <77437076+LIT-RoisinBent@users.noreply.github.com>
2021-02-04 09:05:09 +0000

    Created inventory
    Key disappears when collided, added to MPC inventory (bool ticked)

---
+++ Assets/inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    public bool gotKey;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gotKey = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "key")
+        {
+            gotKey = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        ;
+    }
+}
